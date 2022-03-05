5658e6c823d29b0bb72be944798d01163caa60ca
ismonsen <ilnyr1001@gmail.com>
2022-03-05 12:36:48 +0400

    lesson 24

---
+++ 24_ReadExcel.py
@@ -0,0 +1,17 @@
+import openpyxl
+
+path = 'data1.xlsx'
+
+wb = openpyxl.load_workbook(path)
+sheet = wb.get_sheet_by_name('Sheet1')
+
+rows = sheet.max_row
+cols = sheet.max_column
+
+print(rows)
+print(cols)
+
+for row in range(1, rows + 1):
+    for col in range(1, cols + 1):
+        print(sheet.cell(row=row, column=col).value, end='\t\t')
+    print()
