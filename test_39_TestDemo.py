476d21c0c5a82efa33bdf0ccce6a10abfd6d75aa
ismonsen <ilnyr1001@gmail.com>
2022-03-09 11:35:05 +0400

    lesson 39

---
+++ test_39_TestDemo.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_method1():
+    print('This is Test Method 1')
+
+
+def test_method2():
+    print('This is Test Method 2')
+
+# pytest -v -s test_39_TestDemo.py - запуск конкретного теста
+# pytest -v -s запуск тестов с выводом на экран
+# pytest простой запуск тестов
